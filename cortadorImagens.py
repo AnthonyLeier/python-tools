157dc92531ae5df8eb314dc49875f9c79be580ff
Anthony Cruz <anthonyleierlw@gmail.com>
2020-09-29 13:47:27 -0300

    Cortador de Imagens Remodelado

--- cortadorImagens.py
+++ cortadorImagens.py
@@ -1,5 +1,13 @@
 from PIL import Image
+import os
 
-image = Image.open("./imagens/convertidas/foto1.png")
-largura, altura = image.size
-image.crop((largura/2, 0, largura, altura)).save("./imagens/corte.png", format="PNG")
+i = 0000
+print("Iniciando corte")
+print("Cortando...")
+
+for imagemName in os.listdir("./prints"):
+    i = i + 1
+    imagem = Image.open('./prints/' + imagemName)
+    imagem.crop((2015, 98, 2655, 993)).save("./" + str(i) + ".png", format="PNG")
+    
+print("Corte finalizado com sucesso")
