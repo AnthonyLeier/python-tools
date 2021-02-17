cd65947c47bd06254922ba4f732facacf7d3ec09
Anthony Cruz <anthonyleierlw@gmail.com>
2021-02-17 16:19:09 -0300

    Lista de Ruas Obtidas

--- cep/mapeamentoCep.py
+++ cep/mapeamentoCep.py
@@ -3,10 +3,20 @@ import json
 import unidecode
 import unicodedata
 
-def buscarCeps():
-    arquivo = open("ruas.txt", "w")
+def ultimoCep():
+    arquivo = open("mapeamento.txt", "r")
+    linhas = arquivo.readlines()
+    arquivo.close()
+    ultimaLinha = linhas[len(linhas)-1]
+    cep = int(ultimaLinha[0:8])
+    print("Ultimo Cep:", cep)
+    return cep
 
-    for i in range(89500000, 89512999):
+def buscarCeps(cep):
+    print("Continuando leitura a partir de:", cep)
+    arquivo = open("mapeamento.txt", "a")
+
+    for i in range(cep+1, 89512999):
         request = requests.get("https://brasilapi.com.br/api/cep/v1/" + str(i))
         resposta = json.loads(request.content)
         try:
@@ -18,5 +28,6 @@ def buscarCeps():
             string = str(i)+"|"+"Erro"+"\n"
             string = unidecode.unidecode(string)
             print(string)
-            arquivo.write(string)
-buscarCeps()
+
+cep = ultimoCep()
+buscarCeps(cep)
